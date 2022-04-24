372138d894ca728f683a9d9d21bfed7beacd8c0e
Jihogrammer <jiho@kakao.com>
2022-04-24 16:09:24 +0900

    🛠 #1 pypi friendly README

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name             = 'creon-plus',
-    version          = '0.0',
-    description      = '대신증권 Creon Plus API',
-    long_description = open('README.md').read(),
-    author           = 'jihogrammer',
-    author_email     = 'jihogrammer@gmail.com',
-    license          = 'MIT',
-    packages         = find_packages(),
-    install_requires = ['pywin32'],
-    classifiers=[
+    name                          = 'creon-plus',
+    version                       = '0.0.1',
+    description                   = '대신증권 Creon Plus API',
+    long_description              = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    author                        = 'jihogrammer',
+    author_email                  = 'jihogrammer@gmail.com',
+    license                       = 'MIT',
+    packages                      = find_packages(),
+    install_requires              = ['pywin32'],
+    classifiers                   = [
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
